d909389ef1037519c05fa0fd24c9938ef1c0ed69
Wagner Pires Ferreira <wagner_pires@icloud.com>
2025-06-04 22:13:53 -0600

    First structure created. Carousel on landing page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Raleway } from "next/font/google";
 import "./globals.css";
+import Top from "./components/top";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,21 +13,30 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
   title: "Your Next site ",
   description: "Get a quote today!",
+  icons: "/favicon.ico",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>)
+{
+
   return (
-    <html lang="en">
+    <html lang="en" className="w-[full] h-min-[100vh] scrollbar-gutter-stable">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${raleway.variable} ${geistMono.variable} font-[var(--font-raleway)] antialiased `}
       >
+        <Top/>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,88 @@
 import Image from "next/image";
+import { title } from "process";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
+  let cards = [
+    {
+      title: "Functionality",
+      text: "Great functionality is our top priority. We ensure your site works flawlessly across all devices and provides an exceptional user experience.",
+      imageSrc: "functionality.jpg",
+    },
+    {
+      title: "Intuitiveness",
+      text: "Your website will be easy to navigate and understand. Users will find what they're looking for quickly and effortlessly.",
+      imageSrc: "intuitiveness.jpg",
+    },
+    {
+      title: "Beauty",
+      text: "We create visually stunning designs that capture your brand's essence while maintaining clean and modern aesthetics.",
+      imageSrc: "beauty.jpg",
+    },
+  ];
+
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="text-6xl font-bold bg-primary rounded-sm p-8 w-4/6 text-center shadow-2xl">
+    <div className="flex flex-col items-center justify-items-center min-h-screen max-w-[100vw] p-8 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {/* <div className="text-6xl font-bold bg-primary rounded-sm p-8 w-4/6 text-center shadow-2xl">
         Your Next Site
       </div>
-      <Image className="w-auto " src='/website.jpg' height={800} width={800} alt="Website illustration" />
+      <Image
+        className="w-auto "
+        src="/website.jpg"
+        height={800}
+        width={800}
+        alt="Website illustration"
+      /> */}
 
-      <div className="flex w-full justify-center gap-8 indent-8">
-        <Card
-          title="Functionality"
-          text="Great functionality is our top priority. We ensure your site works flawlessly across all devices and provides an exceptional user experience."
-          imageSrc="functionality.jpg"
-        />
-        <Card
-          title="Intuitiveness"
-          text="Your website will be easy to navigate and understand. Users will find what they're looking for quickly and effortlessly."
-          imageSrc="intuitiveness.jpg"
-        />
-        <Card
-          title="Beauty"
-          text="We create visually stunning designs that capture your brand's essence while maintaining clean and modern aesthetics."
-          imageSrc="beauty.jpg"  // Add an image for beauty card
-        />
-      </div>
+      <Carousel
+      
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      className="w-full max-w-[70vw]"
+      >
+        <CarouselContent>
+          {/* <CarouselItem>...</CarouselItem> */}
+          {/* <CarouselItem>...</CarouselItem> */}
+
+      {/* <div className="flex w-full justify-center gap-8 indent-8"> */}
+        {cards?.map((card, index) => (
+          <CarouselItem key={index}>
+            <Card
+              title={card.title}
+              text={card.text}
+              imageSrc={card.imageSrc}
+            />
+          </CarouselItem>
+        ))}
+      {/* </div> */}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
 
-function Card({ title, text, imageSrc }: { title: string, text: string, imageSrc: string }) {
+function Card({
+  title,
+  text,
+  imageSrc,
+}: {
+  title: string;
+  text: string;
+  imageSrc: string;
+}) {
   return (
     <div
-      className={`flex flex-col items-center w-84 rounded-sm h-96 shadow-lg p-4 text-white relative overflow-hidden`}
+      className={`flex flex-col items-center rounded-sm h-96 shadow-lg p-4 text-white relative overflow-hidden w-max-10/12`}
     >
       <Image
         className="absolute inset-0 w-full h-full object-cover -z-10"
