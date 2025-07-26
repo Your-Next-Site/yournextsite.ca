7ae586f806d4a5fc9b05310ec0a4233bfbe30397
Patrick MacDonald <patrick@patmac.ca>
2025-07-25 18:22:30 -0600

    added google tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono, Raleway } from "next/font/google";
 import "./globals.css";
 import Top from "../components/ui/top";
-
+import Script from 'next/script'
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
 //   subsets: ["latin"],
@@ -22,29 +22,41 @@ export const metadata: Metadata = {
   title: "Your Next site ",
   description: "Get a quote today!",
   icons: "/favicon.ico",
-   keywords: [
+  keywords: [
     "your next website",
     "build a site",
     "make your site",
     "launch a web site",
     "your next site"
-   ],
+  ],
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>)
-{
+}>) {
 
   return (
     <html lang="en" className="w-full h-min-[100vh] scrollbar-gutter-stable">
       <body
         className={`${raleway.variable} ${geistMono.variable} w-full font-[var(--font-raleway)] antialiased `}
       >
-        <Top/>
+        <Top />
         {children}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17279681348"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-global-config" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17279681348');
+          `}
+        </Script>
       </body>
     </html>
   );
